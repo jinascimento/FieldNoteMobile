6355e4cac8909a018fb07c354be094938bf4dee0
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-11-30 16:00:13 -0300

    Reactotron config

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@
  */
 
 import React from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import './config/ReactotronConfig';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
