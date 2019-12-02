876d038ec9c8fcb196d78f08a542820b93c42873
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-12-02 12:02:56 -0300

    Add react-native-maps with ios configs

--- ios/FieldNoteMobile/AppDelegate.m
+++ ios/FieldNoteMobile/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAedn9omprE5VLdxP6BHdS_PN_1Ydhi0Dg%"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"FieldNoteMobile"
